5b48eeee55b99b4c787f49f0030f7c3c73b1e45a
vinidiias <vininoites8350@gmail.com>
2024-09-22 10:07:02 -0300

    fro

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'; // Certifique-se de importar render e screen
 import App from './App'
+import { act } from 'react';
 
 test('renders learn react link', () => {
   render(<App />);
