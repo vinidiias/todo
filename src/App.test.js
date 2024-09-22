c6d593b73762c3f1a5391beecc2964b50a73de97
vinidiias <vininoites8350@gmail.com>
2024-09-22 10:21:20 -0300

    o

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders NavBar, ToDo, and Footer components', () => {
   render(<App />);
-  
-  const linkElement = screen.getByText((content, element) => content.includes('learn react')); // Matcher personalizado
-  expect(linkElement).toBeInTheDocument();
+
+  // Verifica se algo específico do NavBar é renderizado
+  const navBarElement = screen.getByText(/navbar/i); // Altere o texto de acordo com o que é renderizado no NavBar
+  expect(navBarElement).toBeInTheDocument();
+
+  // Verifica se algo específico do ToDo é renderizado
+  const todoElement = screen.getByText(/todo/i); // Altere o texto de acordo com o que é renderizado no ToDo
+  expect(todoElement).toBeInTheDocument();
+
+  // Verifica se algo específico do Footer é renderizado
+  const footerElement = screen.getByText(/footer/i); // Altere o texto de acordo com o que é renderizado no Footer
+  expect(footerElement).toBeInTheDocument();
 });
