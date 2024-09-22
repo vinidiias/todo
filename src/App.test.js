2674ee2d2c1d79fc56cb3b558c288b1ec5de9978
vinidiias <vininoites8350@gmail.com>
2024-09-22 10:12:27 -0300

    fro

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import { act } from 'react'; // Corrigir: importar act diretamente de react
 import App from './App';
 
 test('renders learn react link', () => {
-  act(() => { // Usando act corretamente
-    render(<App />);
-  });
-  const linkElement = screen.getByText(/learn react/i);
+  render(<App />);
+  
+  const linkElement = screen.getByText((content, element) => content.includes('learn react')); // Matcher personalizado
   expect(linkElement).toBeInTheDocument();
 });
