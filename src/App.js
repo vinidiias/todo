e05e8379de356afda167835725000ccd39e4145f
vinidiias <vininoites8350@gmail.com>
2024-09-22 00:16:27 -0300

    o

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import Footer from './components/layout/Footer'
-import NavBar from './components/layout/NavBar'
-import ToDo from './pages/ToDo'
+
 
 function App() {
   return (
