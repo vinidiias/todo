a854f50ce06325972825053b6cc77c59c2768159
vinidiias <vininoites8350@gmail.com>
2024-09-22 10:08:51 -0300

    fro

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import { render, screen } from '@testing-library/react'; // Certifique-se de importar render e screen
-import App from './App'
-import { act } from 'react';
+import { render, screen } from '@testing-library/react';
+import { act } from 'react'; // Corrigir: importar act diretamente de react
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
+  act(() => { // Usando act corretamente
+    render(<App />);
+  });
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
