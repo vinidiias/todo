a32b2b7fbe4faf5e6610c35f1f3eb11539f40294
vinidiias <vininoites8350@gmail.com>
2024-09-22 10:18:39 -0300

    o

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
+import Footer from './components/layout/Footer'
+import NavBar from './components/layout/NavBar'
+import ToDo from './pages/ToDo'
 
 function App() {
   return (
     <div className="app">
-      <p>oi</p>
+      <NavBar />
+      <ToDo />
+      <Footer />
     </div>
   );
 }
